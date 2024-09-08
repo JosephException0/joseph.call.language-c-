455ac1a070af9568963d8af1690b4f790b4fa8e7
JosephException0 <josephvictornovabos@gmail.com>
2024-09-08 20:47:41 +0800

    Create struct.h

---
+++ struct.h
@@ -0,0 +1,20 @@
+#ifndef STRUCT_H
+#define STRUCT_H
+
+typedef struct zample {
+    char tag;
+    int count;
+    double value;
+} zample;
+
+typedef struct killa {
+    float x;
+    double intensity;
+    int red;
+    long long timestamp;
+    int green;
+    float y;
+    int blue;
+} killa;
+
+#endif // STRUCT_H
